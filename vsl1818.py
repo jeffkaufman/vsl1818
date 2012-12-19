507162ef61085fb02f27d22cd8b4c47ab9f8f457
Jeff Kaufman <jeff@jefftk.com>
2012-12-19 14:40:32 -0500

    steps toward usable interface

--- vsl1818.py
+++ vsl1818.py
@@ -7,6 +7,8 @@ import wsgiref.simple_server
 import wsgiref.util
 import threading
 import traceback
+import time
+import json
 
 HOST='localhost'
 PORT=7069
@@ -17,29 +19,22 @@ def parsestr(s, n=None):
         assert len(s) == n
     return s[:s.find('\x00')]
 
-def init_controls():
-    control_table = [
-        (3000, "master"),
-        (1, "master pan"),
-        (3005,"aux 3-4"),
-        (16,"pan 3-4"),
-        (3007,"aux 5-6"),
-        (17,"pan 5-6"),
-        (3009,"aux 7-8"),
-        (17,"pan 7-8"),
-        (3013, "A"),
-        (3014, "B"),
-        (3052, "mute"),
-        (62, "phase reverse"),
-        (3021, "high pass"),
-        (3063, "post"),
-        ]
-    for control_encoded, control_decoded in control_table:
-        control_encode[control_decoded] = control_encoded
-        control_decode[control_encoded] = control_decoded
-control_encode = {}
-control_decode = {}
-init_controls()
+control_decode = {
+    3000: "master",
+    1: "master pan",
+    3005: "aux 3-4",
+    16: "pan 3-4",
+    3007: "aux 5-6",
+    17: "pan 5-6",
+    3009: "aux 7-8",
+    18: "pan 7-8",
+    3013: "A",
+    3014: "B",
+    3052: "mute",
+    62: "phase reverse",
+    3021: "high pass",
+    3063: "post",
+    }
 
 class VSL1818(object):
     def __init__(self):
@@ -49,7 +44,7 @@ class VSL1818(object):
         self.levels = None
         self.channel_display_names = {}
 
-        # channel_name -> control_name -> value
+        # channel -> control -> value
         self.channels = {}
 
     def update(self, message_header, message_body):
@@ -68,7 +63,7 @@ class VSL1818(object):
             self.channel_display_names[channel_num] = parsestr(channel_display_name)
 
         elif category == 2:
-            control_raw, value, channel = struct.unpack("=Hd32s", message_body)
+            control, value, channel = struct.unpack("=Hd32s", message_body)
             channel = parsestr(channel).split(",")[0]
             if channel.startswith("fx "):
                 return
@@ -77,10 +72,10 @@ class VSL1818(object):
 
             if channel not in self.channels:
                 self.channels[channel] = {}
-
-            if control_raw not in control_decode:
+            
+            if control not in control_decode:
                 return # ignoring unknown controls for now
-            control = control_decode[control_raw]
+
             self.channels[channel][control] = value
 
         else:
@@ -108,7 +103,7 @@ class VSL1818(object):
             for control, value in sorted(controls.iteritems()):
                 if not s[-1].startswith("<tr>"):
                     s.append("<tr>")
-                s.append("<td>%s<td>%s" % (control, value))
+                s.append("<td>%s<td>%s" % (control_decode[control], value))
         s.append("</table>")
         return "".join(s)
 
@@ -160,11 +155,36 @@ def output(request, vsl):
     display_name = vsl.channel_display_names[channel]
 
     s = ["<html>", "<h1>Channel %s</h1>" % display_name]
+    s.append("<style>"
+             ".barfg{background-color:black;margin:0;padding:0}"
+             ".barbg{background-color:#BBB;margin:0;padding:0}"
+             "</style>")
     controls = vsl.channels[channel]
     s.append("<dl>")
     for control, value in sorted(controls.iteritems()):
-        s.append("<dt>%s<dd>%s" % (control, value))
+        s.append("<dt>%s" % control_decode[control])
+        s.append('<dd>'
+                 '<div id="control-bg-%s" class="barbg">'
+                 '<div id="control-fg-%s" class="barfg" style="width:%.2f%%"'
+                 '>&nbsp;</div></div>' % (control, control, value*100))
     s.append("</dl>")
+    s.append("<script>"
+             "function click_handler(fg, bg, control, channel) {"
+             "  return function(e) {"
+             "     var value = e.offsetX/bg.offsetWidth;"
+             "     /* todo: send to server via ajax */"
+             "     console.debug('channel='+channel+' control=' + control + 'value=' + value);"
+             "  }"
+             "}"
+             "var channel=%s;"
+             "var controls=%s;"
+             "var i = 0;"
+             "for (i = 0 ; i < controls.length ; i++) {"
+             "  var bg = document.getElementById('control-bg-' + controls[i]);"
+             "  var fg = document.getElementById('control-fg-' + controls[i]);"
+             "  bg.onclick = click_handler(fg, bg, controls[i], channel);"
+             "}"
+             "</script>" % (channel, json.dumps(list(controls))))
     return "".join(s)
 
 def handle_request(request, vsl):
@@ -181,11 +201,42 @@ def handle_request(request, vsl):
         return vsl.dumpstate()
     raise Exception("Unknown Request Path: '%s'" % request)
 
-def start(args):
-    assert not args
+class MockVSL():
+    def __init__(self):
+        self.loaded = False
+        self.killed = False
+        self.levels = None
+        self.channel_display_names = {1: "main", 2: "aux"}
 
-    vsl = VSL1818()
-    vsl.connect()
+        # channel_name -> control_name -> value
+        self.channels = {1:{}, 2:{}}
+
+    def update_always(self):
+        i = 0
+        while not self.killed:
+            c = float(i % 256)
+            
+            self.levels = (c, c, 256-c, 256-c)
+            self.channels[1][3000] = 0.0
+            self.channels[1][3005] = .1
+            self.channels[1][3007] = .5
+            self.channels[1][3009] = .9
+            self.channels[1][1] = 1.0
+
+            time.sleep(1)
+            i += 1
+
+    def dumpstate(self):
+        return "dumpstate"
+
+def start(args):
+    if len(args) == 1 and args[0] == "mock":
+        vsl = MockVSL()
+    else:
+        assert not args
+
+        vsl = VSL1818()
+        vsl.connect()
 
     update_thread = threading.Thread(target=vsl.update_always)
     update_thread.start()
@@ -201,11 +252,6 @@ def start(args):
             headers = [('Content-type', 'text/plain')]
             start_response("500 Internal Server Error", headers)
             return tb
-
-
-        #return wsgiref.util.request_uri(environ)
-        #return vsl.dumpstate()
-        
 
     httpd = wsgiref.simple_server.make_server('', 8000, vsl_app)
     print "Serving HTTP on port 8000..."
