9ffdbcb8949c469d495e474b6a2422f603dbec9a
Jeff Kaufman <jeff@jefftk.com>
2012-12-18 22:01:52 -0500

    parsing the rest

--- vsl1818.py
+++ vsl1818.py
@@ -2,6 +2,7 @@ import sys
 import re
 import datetime
 import struct
+from socket import *
 
 def pp(rb):
     return " ".join(b.encode("hex") for b in rb)
@@ -12,7 +13,34 @@ def parsestr(s, n=None):
         assert len(s) == n
     return s[:s.find('\x00')]
 
+def init_controls():
+    control_table = [
+        (3000, "master"),
+        (1, "master pan"),
+        (3005,"aux 3-4"),
+        (16,"pan 3-4"),
+        (3007,"aux 5-6"),
+        (17,"pan 5-6"),
+        (3009,"aux 7-8"),
+        (17,"pan 7-8"),
+        (3013, "A"),
+        (3014, "B"),
+        (3052, "mute"),
+        (62, "phase reverse"),
+        (3021, "high pass"),
+        (3063, "post"),
+        ]
+    for control_encoded, control_decoded in control_table:
+        control_encode[control_decoded] = control_encoded
+        control_decode[control_encoded] = control_decoded
+
+control_encode = {}
+control_decode = {}
+init_controls()
+
 class Record(object):
+    control_table = []
+
     def __init__(self, v, r):
         unknown1, unknown2, self.category, unknown3 = struct.unpack("IIHH", v)
         assert unknown1 == 0x01020103
@@ -22,16 +50,18 @@ class Record(object):
         self.channel = ''
 
         if self.category == 5:
-            assert r == '\x00'*128
+            self.levels = struct.unpack("128B", r)
 
         elif self.category == 4:
-            unknown4, self.e1, channel = struct.unpack("HH48s", r)
+            unknown4, self.output_num, channel = struct.unpack("HH48s", r)
             assert unknown4 == 0
             self.channel = parsestr(channel)
 
         elif self.category == 2:
-            self.c1, channel = struct.unpack("10s32s", r)
+            control_raw, self.value, channel = struct.unpack("=Hd32s", r)
             self.channel = parsestr(channel)
+            self.control_parsed = control_decode.get(control_raw, control_raw)
+            
         else:
             raise Exception("unknown category %s" % self.category)
 
@@ -40,11 +70,11 @@ class Record(object):
 
     def __str__(self):
         if self.category == 5:
-            return "5: all zeros"
+            return "5: %s" % (self.levels, )
         elif self.category == 4:
-            return "4: %s %s" % (self.e1, self.channel)
+            return "4: %s %s" % (self.output_num, self.channel)
         elif self.category == 2:
-            return "2: %s %s" % (pp(self.c1), self.channel)
+            return "2: %s %s %s" % (self.control_parsed, self.value, self.channel)
         else:
             assert False
 
@@ -62,13 +92,18 @@ class Record(object):
             yield Record(v[:12], v[12:])
 
 class Tee(object):
-    def __init__(self, fname_out, inf):
-        self.inf = inf
+    def __init__(self, fname_out, sock):
+        self.sock = sock
         self.outf = open(fname_out, "w")
 
-    def read(*args, **kwargs):
-        s = self.inf.read(*args, **kwargs)
-        self.outf.write(s)
+    def read(self, n):
+        s = ""
+        def remaining():
+            return n-len(s)
+        while remaining() > 0:
+            b = self.sock.recv(remaining())
+            self.outf.write(b)
+            s += b
         return s
 
 def nowstr():
@@ -91,6 +126,7 @@ def connection_record():
 def connect_tcp(host, port):
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((host, port))
+    return s
 
 def should_connect(data):
     (r, ) = list(Record.readall(data))
@@ -103,7 +139,8 @@ def start(args):
 
         bs = Tee("log-%s.bytes" % nowstr(), s)
         for r in Record.readall(bs):
-            print str(r)
+            if r.category != 5:
+                print str(r)
     else:
         (fname, ) = args
 
