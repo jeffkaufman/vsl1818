09c015ace1033ce0c86a628f13cc11dfe9e4f06f
Jeff Kaufman <jeff@jefftk.com>
2012-12-19 08:57:13 -0500

    conveted to use web interface

--- vsl1818.py
+++ vsl1818.py
@@ -3,6 +3,10 @@ import re
 import datetime
 import struct
 from socket import *
+import wsgiref.simple_server
+import wsgiref.util
+import threading
+import traceback
 
 HOST='localhost'
 PORT=7069
@@ -39,6 +43,8 @@ init_controls()
 
 class VSL1818(object):
     def __init__(self):
+        self.loaded = False
+        self.killed = False
         self.connection = None
         self.levels = None
         self.channel_display_names = {}
@@ -53,6 +59,7 @@ class VSL1818(object):
         assert unknown3 == 10
         
         if category == 5:
+            self.loaded = True # we're not fully loaded until we get some levels
             self.levels = struct.unpack("128B", message_body)
 
         elif category == 4:
@@ -70,7 +77,10 @@ class VSL1818(object):
 
             if channel not in self.channels:
                 self.channels[channel] = {}
-            control = control_decode.get(control_raw, control_raw)
+
+            if control_raw not in control_decode:
+                return # ignoring unknown controls for now
+            control = control_decode[control_raw]
             self.channels[channel][control] = value
 
         else:
@@ -79,17 +89,28 @@ class VSL1818(object):
     def update_always(self):
         assert self.connection
 
-        while True:
+        while not self.killed:
             signature, l = struct.unpack("II", self.read(8))
             assert signature == 0xaa550011
             v = self.read(l)
             self.update(v[:12], v[12:])
 
     def dumpstate(self):
+        if not self.loaded:
+            return "<p>loading...</p>"
+
+        s = ["<table border=1>"]
         for channel, controls in sorted(self.channels.iteritems()):
-            print self.channel_display_names.get(channel, channel)
+            if channel not in self.channel_display_names:
+                continue # ignoring unknown channels for now
+            display_name = self.channel_display_names[channel]
+            s.append("<tr><td rowspan=%s>%s" % (len(controls), display_name))
             for control, value in sorted(controls.iteritems()):
-                print " ", control, value
+                if not s[-1].startswith("<tr>"):
+                    s.append("<tr>")
+                s.append("<td>%s<td>%s" % (control, value))
+        s.append("</table>")
+        return "".join(s)
 
     def read(self, n):
         s = ""
@@ -117,15 +138,82 @@ class VSL1818(object):
         self.connection.send(struct.pack("II", 0xaa550011, len(message)))
         self.connection.send(message)
 
+def index(vsl):
+    s = ["<html>", "<h1>VSL 1818</h1>"]
+    for text, target in [["Dump State", "/dump"],
+                         ["List Outputs", "/outputs"]]:
+        s.append('<a href="%s">%s</a><br>' % (target, text))
+    return "".join(s)
+
+def list_outputs(vsl):
+    s = ["<html>", "<h1>Available Channels</h1>"]
+    for channel in sorted(vsl.channels):
+        if channel not in vsl.channel_display_names:
+            continue # ignoring unknown channels for now
+        display_name = vsl.channel_display_names[channel]
+        s.append('<a href="/output/%s">%s</a><br>' % (channel, display_name))
+    return "".join(s)
+
+def output(request, vsl):
+    (channel_raw,) = request
+    channel = int(channel_raw)
+    display_name = vsl.channel_display_names[channel]
+
+    s = ["<html>", "<h1>Channel %s</h1>" % display_name]
+    controls = vsl.channels[channel]
+    s.append("<dl>")
+    for control, value in sorted(controls.iteritems()):
+        s.append("<dt>%s<dd>%s" % (control, value))
+    s.append("</dl>")
+    return "".join(s)
+
+def handle_request(request, vsl):
+    assert not request[0]
+    request = request[1:]
+
+    if not request[0]:
+        return index(vsl)
+    if request == ['outputs']:
+        return list_outputs(vsl)
+    if request[0] == "output":
+        return output(request[1:], vsl)
+    if request == ['dump']:
+        return vsl.dumpstate()
+    raise Exception("Unknown Request Path: '%s'" % request)
+
 def start(args):
     assert not args
-    
+
     vsl = VSL1818()
     vsl.connect()
+
+    update_thread = threading.Thread(target=vsl.update_always)
+    update_thread.start()
+
+    def vsl_app(environ, start_response):
+        try:
+            response = handle_request(environ["PATH_INFO"].split("/"), vsl)
+            headers = [('Content-type', 'text/html')]
+            start_response("200 OK", headers)
+            return str(response)
+        except Exception:
+            tb = traceback.format_exc()
+            headers = [('Content-type', 'text/plain')]
+            start_response("500 Internal Server Error", headers)
+            return tb
+
+
+        #return wsgiref.util.request_uri(environ)
+        #return vsl.dumpstate()
+        
+
+    httpd = wsgiref.simple_server.make_server('', 8000, vsl_app)
+    print "Serving HTTP on port 8000..."
+
     try:
-        vsl.update_always()
+        httpd.serve_forever()
     except:
-        vsl.dumpstate()
+        vsl.killed = True
         raise
 
 if __name__ == "__main__":
